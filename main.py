7d1549901bc1bbf033b04e21d14a4063f5fe1ea4
joel.perez.bermudez <joel.perez.bermudez@estudiantat.upc.edu>
2025-09-26 11:19:23 +0200

    chore: initialize project structure with folders and placeholder files

---
+++ main.py
@@ -0,0 +1,8 @@
+from models.detector import Detector
+from models.classifier import Classifier
+from models.corrector import Corrector
+
+
+detector = Detector()
+classifier = Classifier()
+corrector = Corrector()
