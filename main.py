3aacc7e743df12dd4d1ce742c172da8dd760ca4d
joel.perez.bermudez <joel.perez.bermudez@estudiantat.upc.edu>
2025-09-26 12:16:14 +0200

    dataset import

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from models.detector import Detector
-from models.classifier import Classifier
-from models.corrector import Corrector
+#from models.detector import Detector
+#from models.classifier import Classifier
+#from models.corrector import Corrector
+import pandas as pd
+import numpy as np
 
 
-detector = Detector()
-classifier = Classifier()
-corrector = Corrector()
+#detector = Detector()
+#classifier = Classifier()
+#corrector = Corrector()
+
+
+dataset = pd.read_csv('data/dataset_cleand-1-1-1.csv')
+
+dataset=dataset[['native', 'text']]
+
+for i in dataset.index:
+    if '<br/>' in dataset['native'][i]:
+        dataset['native'][i]=dataset['native'][i].split('<br/>')
+    else:
+        dataset['native'][i]=np.array([dataset['native'][i]])
+
+print(dataset.head())
